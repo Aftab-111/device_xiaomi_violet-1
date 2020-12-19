58bc007d82e7422caf66c96a2cfb31cf88ee751c
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    violet: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,17 +25,17 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-arrow_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${arrow_ROOT}/vendor/arrow/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE}" "${VENDOR}" "${arrow_ROOT}" false
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
 
 # Copyright headers and guards
 write_headers "${DEVICE}"
